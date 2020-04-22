d0f1c8599a249bfd90cfda2afe04c9469fddd9d7
Michael Gallup <Gallup93@icloud.com>
2020-04-22 09:13:28 -0600

    added assertion_of_instance test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,11 @@ require './lib/ingredient'
 require './lib/pantry'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_initialization_with_attributes
+    assert_instance_of Pantry, @pantry
+  end
 end
