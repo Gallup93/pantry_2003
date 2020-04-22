2441a66ef0b1ceabbe511cf2f0dc605035eb95ed
Michael Gallup <Gallup93@icloud.com>
2020-04-22 09:19:56 -0600

    added test for check_stock with no stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -6,10 +6,15 @@ require './lib/pantry'
 class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
   end
 
   def test_initialization_with_attributes
     assert_instance_of Pantry, @pantry
     assert_equal ({}), @pantry.stock
+  end
+
+  def test_stock_check_with_no_stock
+    assert_equal ({}), @pantry.stock_check(@ingredient1)
   end
 end
