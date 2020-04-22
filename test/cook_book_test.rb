e61e8c56fba70e615d4c797d188188b0714b3cd0
Michael Gallup <Gallup93@icloud.com>
2020-04-22 09:58:48 -0600

    added CookBook initialize

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/recipe'
+require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
   def setup
