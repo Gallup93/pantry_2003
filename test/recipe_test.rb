e425c81ef5c177c8a3a1988c26c88f35841cacda
Michael Gallup <Gallup93@icloud.com>
2020-04-22 09:35:06 -0600

    added initialize and test (no attributes)

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -4,4 +4,13 @@ require './lib/ingredient'
 require './lib/recipe'
 
 class RecipeTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+
+  def test_initialization_with_attributes
+    assert_instance_of Recipe, @recipe1
+  end
 end
