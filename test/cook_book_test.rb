6a58f1fa9f794c2844d574622577516e343774a5
Michael Gallup <Gallup93@icloud.com>
2020-04-22 09:56:48 -0600

    added CookBook initialize test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -4,4 +4,12 @@ require './lib/ingredient'
 require './lib/recipe'
 
 class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+  end
+
+  def test_initialization_with_attributes
+    assert_instance_of CookBook, @cookbook
+    assert_equal ([]), @cookbook.recipes
+  end
 end
